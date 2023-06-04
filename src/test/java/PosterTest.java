c7d7e4a3df1324a75b451eb7779561947774d1f6
Karina <kulinicheva_91@mail.ru>
2023-06-04 19:17:36 +0300

    making changes to tests

--- src/test/java/PosterTest.java
+++ src/test/java/PosterTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PosterTest {
+    PosterManager posterManager = new PosterManager();
 
     @Test
     public void addTest() {
-        PosterManager posterManager = new PosterManager();
         PosterDTO poster = new PosterDTO(1, "film 1", "soap");
         posterManager.add(poster);
 
@@ -17,7 +17,6 @@ public class PosterTest {
     @Test
     public void findLastTest() {
         int count = 5;
-        PosterManager posterManager = new PosterManager();
         PosterDTO[] posters = {
             new PosterDTO(1, "film 1", "soap"),
             new PosterDTO(2, "film 2", "soap"),
@@ -27,40 +26,46 @@ public class PosterTest {
             new PosterDTO(6, "film 6", "soap"),
         };
 
-        for (PosterDTO poster: posters) {
-            posterManager.add(poster);
-        }
+        PosterDTO[] lastPosterExpected = {
+                posters[5],
+                posters[4],
+                posters[3],
+                posters[2],
+                posters[1],
+        };
 
-        PosterDTO[] lastPosterExpected = new PosterDTO[count];
-        for (int i = 0; i < lastPosterExpected.length; i++) {
-            lastPosterExpected[i] = posters[posters.length - 1 - i];
-            if (i == count - 1) {
-                break;
-            }
-        }
+        posterManager.add(posters[0]);
+        posterManager.add(posters[1]);
+        posterManager.add(posters[2]);
+        posterManager.add(posters[3]);
+        posterManager.add(posters[4]);
+        posterManager.add(posters[5]);
+
         Assertions.assertEquals(count, posterManager.findLast().length);
         Assertions.assertArrayEquals(lastPosterExpected, posterManager.findLast());
     }
+
     @Test
     public void findLastMinTest() {
         int count = 5;
-        PosterManager posterManager = new PosterManager();
+
         PosterDTO[] posters = {
-            new PosterDTO(1, "film 1", "soap"),
-            new PosterDTO(2, "film 2", "soap"),
-            new PosterDTO(3, "film 3", "soap")
+                new PosterDTO(1, "film 1", "soap"),
+                new PosterDTO(2, "film 2", "soap"),
+                new PosterDTO(3, "film 3", "soap")
         };
 
-        for (PosterDTO poster: posters) {
-            posterManager.add(poster);
-        }
+        PosterDTO[] lastPosterExpected = {
+                posters[2],
+                posters[1],
+                posters[0]
+        };
 
-        PosterDTO[] lastPosterExpected = new PosterDTO[Math.min(posters.length, count)];
-        for (int i = 0; i < lastPosterExpected.length; i++) {
-            if (i <= count - 1) {
-                lastPosterExpected[i] = posters[posters.length - 1 - i];
-            }
-        }
+        posterManager.add(posters[0]);
+        posterManager.add(posters[1]);
+        posterManager.add(posters[2]);
+
+
         Assertions.assertEquals(3, posterManager.findLast().length);
         Assertions.assertArrayEquals(lastPosterExpected, posterManager.findLast());
     }
@@ -78,25 +83,26 @@ public class PosterTest {
         int count = 4;
         PosterManager posterManager = new PosterManager(count);
         PosterDTO[] posters = {
-            new PosterDTO(1, "film 1", "soap"),
-            new PosterDTO(2, "film 2", "soap"),
-            new PosterDTO(3, "film 3", "soap"),
-            new PosterDTO(4, "film 4", "soap"),
-            new PosterDTO(5, "film 5", "soap"),
-            new PosterDTO(6, "film 6", "soap"),
+                new PosterDTO(1, "film 1", "soap"),
+                new PosterDTO(2, "film 2", "soap"),
+                new PosterDTO(3, "film 3", "soap"),
+                new PosterDTO(4, "film 4", "soap"),
+                new PosterDTO(5, "film 5", "soap"),
+                new PosterDTO(6, "film 6", "soap"),
         };
 
-        for (PosterDTO poster: posters) {
-            posterManager.add(poster);
-        }
+        PosterDTO[] lastPosterExpected = {
+                posters[3],
+                posters[2],
+                posters[1],
+                posters[0],
+        };
 
-        PosterDTO[] lastPosterExpected = new PosterDTO[count];
-        for (int i = 0; i < lastPosterExpected.length; i++) {
-            lastPosterExpected[i] = posters[posters.length - 1 - i];
-            if (i == count - 1) {
-                break;
-            }
-        }
+        posterManager.add(posters[0]);
+        posterManager.add(posters[1]);
+        posterManager.add(posters[2]);
+        posterManager.add(posters[3]);
+
         Assertions.assertEquals(count, posterManager.findLast().length);
         Assertions.assertArrayEquals(lastPosterExpected, posterManager.findLast());
     }
